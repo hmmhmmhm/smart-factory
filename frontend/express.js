cb8d90e265338009ce993b596c02ce6b5e0b21a5
hmmhmmhm <hmmhmmhm@naver.com>
2019-03-27 14:41:58 +0900

    Added TSLint

--- frontend/express.js
+++ frontend/express.js
@@ -3,7 +3,9 @@ const app = express();
 const portNumber = 3000;
 const sourceDir = 'dist';
 
-app.use(express.static(sourceDir));
+app.use("/", express.static(sourceDir))
+app.use("/about", express.static(sourceDir))
+app.use("/unknown", express.static(sourceDir))
 
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
